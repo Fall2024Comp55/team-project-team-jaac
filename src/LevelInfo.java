24ff0277a9d4d12d6442d256cab9d3429080b3ee
alexshome <alexshome@dhcp-10-15-164-26.wireless.pacific.edu>
2024-11-28 17:25:31 -0800

    Fixed player Lane positioning in Level 3

--- src/LevelInfo.java
+++ src/LevelInfo.java
@@ -33,8 +33,8 @@ public class LevelInfo {
 		} else if (level == 3) { // Add Level 3 here
 	        info = new LevelInfo(
 	                "media/images/playing/level3landscape 2.png",
-	                new int[] { 150, 350, 550, 750, 950 }, // More lanes for increased difficulty
-	                0.3, 1.6, 60, 2); // Faster speed, higher density, and more vehicles to pass
+	                new int[] { 150, 350, 520, 720, 902 }, // More lanes for increased difficulty
+	                0.3, 1.5, 60, 2); // Faster speed, higher density, and more vehicles to pass
 	        }
 
 		return info;

feb65b7aa9cef1d8e5843b1fd6cb13fd504d610d
Yanjun1337 <y_deng1@u.pacific.edu>
2024-11-30 13:33:25 +0800

    Added Data Storage Function

--- src/PlayerData.java
+++ src/PlayerData.java
@@ -1,12 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 
 // This singleton class is used to store data about the player
 // Maybe in the future this information will be stored in a file
 // Use this class:
 //   PlayerData.getInstance().method();
-public class PlayerData {
+public class PlayerData implements Serializable {
 
-    public static class LevelScore {
+    public static class LevelScore implements Serializable {
         public int star;
         public long timeMs;
         public int carPassed;
@@ -15,8 +21,14 @@ public class PlayerData {
 	private HashMap<Integer, LevelScore> bestScores;
 
 	private PlayerData() {
-		// Read from file?
-        bestScores = new HashMap<Integer, LevelScore>();
+		// Read from file
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.ser"))) {
+            PlayerData obj = (PlayerData)in.readObject();
+            this.bestScores = obj.bestScores;
+        } catch (IOException | ClassNotFoundException e) {
+            bestScores = new HashMap<Integer, LevelScore>();
+            //e.printStackTrace();
+        }
 	}
 
     public LevelScore getBestScore(int level) {
@@ -25,6 +37,13 @@ public class PlayerData {
 
     public void setBestScore(int level, LevelScore score) {
     	bestScores.put(level, score);
+
+        // Save to file
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.ser"))) {
+            out.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 	private static class PlayerDataHolder {

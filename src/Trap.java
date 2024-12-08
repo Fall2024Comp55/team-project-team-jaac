3444ca46b15f095018cd7548b360f0d02a71ed31
alexshome <alexshome@dhcp-10-15-165-62.wireless.pacific.edu>
2024-12-07 16:59:44 -0800

    getImage for Trap class

--- src/Trap.java
+++ src/Trap.java
@@ -1,4 +1,27 @@
+import acm.graphics.GImage;
 
 public class Trap {
+    private GImage image;
+    private int lane;
 
+    public Trap(String imagePath, int x, int y, int lane) {
+        this.image = new GImage(imagePath, x, y);
+        this.lane = lane;
+    }
+
+    public GImage getImage() {
+        return image;
+    }
+
+    public int getLane() {
+        return lane;
+    }
+
+    public int getY() {
+        return (int) image.getY();
+    }
+
+    public void move(int deltaY) {
+        image.move(0, deltaY);
+    }
 }
